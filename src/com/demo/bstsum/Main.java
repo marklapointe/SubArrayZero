2a1dbe656597b74ab037d5be881ab5e3e820834e
Mark LaPointe <mark.lapointe@redwolfsecurity.com>
2020-07-10 13:56:23 -0400

    Init

---
+++ src/com/demo/bstsum/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.demo.bstsum;
+
+/**
+ * @author marklapointe
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Start");
+	}
+
+}
