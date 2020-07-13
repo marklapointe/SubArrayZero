2a42f872c5dfdfe0d9cca5a6d40c2151ed036503
Mark LaPointe <mark@revynet.org>
2020-07-12 21:10:48 -0400

    Made implementation

--- src/com/demo/arraythings/Main.java
+++ src/com/demo/arraythings/Main.java
@@ -1,9 +1,15 @@
 /**
  * 
  */
-package com.demo.bstsum;
+package com.demo.arraythings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * 
+ * Given an array of integers, find the length of the longest sub-array with sum equals to 0.
+ * 
  * @author marklapointe
  *
  */
@@ -13,7 +19,7 @@ public class Main {
 	 * 
 	 */
 	public Main() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -21,6 +27,193 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Start");
+		Node root = new Node(10);
+		root.ordered_insert(1);
+		root.ordered_insert(-1);
+		root.ordered_insert(11);
+		root.ordered_insert(77);
+		
+		Permutation permutation = root.findLongestPermutation(0);
+		System.out.println(permutation.toString());
+		
+		Permutation permutation2 = root.findLongestPermutation(21);
+		System.out.println(permutation2.toString());
+		
+		Permutation permutation3 = root.findLongestPermutation(98);
+		System.out.println(permutation3.toString());
+		
+		
+		Permutation permutation4 = root.findLongestPermutation(1000000);
+		if (permutation4 != null) {
+			System.out.println("BAD: " + permutation4.toString());
+		} else {
+			System.out.println("OK for test 4");
+		}
+		
+		System.out.println("End");
 	}
 
+}
+
+class Permutation {
+	Integer[] perm;
+	Integer value;
+	
+	public Permutation(List<Integer> perm) {
+		this.perm = new Integer[perm.size()];
+		for ( int i = 0; i < perm.size(); i++) {
+			this.perm[i] = perm.get(i);
+		}
+		this.sum();
+	}
+	
+	@Override
+	public String toString() {
+		String result = "[";
+		for (int i = 0; i < this.length(); i++) {
+			result += this.perm[i];
+			if (i < (this.length() -1)) {
+				result += ",";
+			}
+		}
+		result += "]";
+		return result;
+	}
+	
+	public Permutation(Integer[] perm) {
+		this.perm = perm;
+		this.sum();
+	}
+	
+	private void sum() {
+		if (this.perm != null) {
+			Integer tmp = 0;
+			for ( Integer i: perm) {
+				tmp += i;
+			}
+			this.value = tmp;
+		}
+	}
+	
+	public int length() {
+		if (perm != null) return perm.length;
+		return 0;
+	}
+	
+}
+
+
+class Node {
+	Integer data;
+	Node left;
+	Node right;
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public Permutation findLongestPermutation (Integer target) {
+		List<Permutation> permutations = new ArrayList<Permutation>();
+		this.getPermutations(permutations,new ArrayList<Integer>(),this.getInOrderInteger());
+		Permutation result = this.findLongest(permutations, target);
+		return result;
+		
+	}
+	
+	public Permutation findLongest (List<Permutation> permutations, Integer target) {
+		Permutation result = null;
+		for (Permutation p: permutations) {
+			if (p.value == target) {
+				if (result == null) result = p;
+				if (p.length() > result.length()) {
+					result = p;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public void getPermutations(List<Permutation> permutations, List<Integer> lead, List<Integer> remainder ) {
+		for (int i = 0; i < remainder.size(); i++) {
+			// Pull a lead
+			Integer leadv = remainder.get(i);
+			List<Integer> nLead = new ArrayList<>();
+			List<Integer> nRemainder = new ArrayList<>();
+			nLead.addAll(lead);
+			nRemainder.addAll(remainder);
+			nLead.add(leadv);
+			nRemainder.remove(i);
+			permutations.add(new Permutation(nLead));
+			if (nRemainder.size() > 0) this.getPermutations(permutations, nLead, nRemainder);
+		}
+		
+	
+	}
+	
+	public Boolean hasValue(Integer value) {
+		if (this.data == value) {
+			return true;
+		}
+		if (this.data < value && this.right != null) return this.right.hasValue(value);
+		if (this.data > value && this.left != null) return this.left.hasValue(value);
+		return false;
+	}
+	
+	public Boolean findTarget(Integer target) {
+		Boolean retVal = false;
+		List<Node> nodes = this.getInOrder();
+		for (int i = 0; i <= (nodes.size()-1) && !retVal; i++) {
+			Integer targetValue = target - nodes.get(i).data;
+			retVal = this.hasValue(targetValue);
+		}
+		
+		return retVal;
+	}
+	
+	private List<Node> getInOrder() {
+		List<Node> retVal = new ArrayList<Node>();
+		if (this.left!= null) retVal.addAll(this.left.getInOrder());
+		retVal.add(this);
+		if (this.right!= null) retVal.addAll(this.right.getInOrder());
+		return retVal;
+	}
+
+	private List<Integer> getInOrderInteger() {
+		List<Integer> retVal = new ArrayList<Integer>();
+		if (this.left!= null) retVal.addAll(this.left.getInOrderInteger());
+		retVal.add(this.data);
+		if (this.right!= null) retVal.addAll(this.right.getInOrderInteger());
+		return retVal;
+	}
+	
+	public void ordered_insert(Integer data) {
+		if (this.data == null) {
+			this.data = data;
+			return;
+		} else if (data > this.data) {
+			this.ordered_insert_right(data);
+			return;
+		} else if (data < this.data) {
+			this.ordered_insert_left(data);
+			return;
+		}
+		return;
+	}
+
+	private void ordered_insert_right (Integer data) {
+		if (this.right == null) {
+			this.right = new Node(data);
+		} else {
+			this.right.ordered_insert(data);
+		}
+	}
+	
+	private void ordered_insert_left (Integer data) {
+		if (this.left == null) {
+			this.left = new Node(data);
+		} else {
+			this.left.ordered_insert(data);
+		}
+	}
+	
 }
